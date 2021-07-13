a75a71c5459ef001dd583ac260ff0415f1f2a497
Cjhightower38 <Cjhightower38@gmail.com>
2021-07-13 19:44:05 -0400

    first commit

---
+++ webpage.py
@@ -0,0 +1,10 @@
+# Import urllib for downloading url's and sys for errors
+import urllib3
+import sys
+
+# Add a dummy url to start the while loop
+url_link = 'http://www.google.com'
+
+# Add empty dictionary to store search findings as key-pair values.
+url_dict = {}
+
